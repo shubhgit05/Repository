c033f734086320ca32b145fe1352c43420cf94ba
shubhgit05 <shubh_arora@epam.com>
2026-02-06 23:32:38 +0530

    add index2.cs

---
+++ index2.cs
@@ -0,0 +1,18 @@
+
+
+
+
+using System ;
+namespace Learning{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#  ");
+            return ;
+        }
+    }
+    
+}
+
