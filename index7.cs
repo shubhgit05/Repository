2afa4079df19e41e12d90df521a5de6c366a9d0e
shubhgit05 <shubh_arora@epam.com>
2026-02-07 14:23:01 +0530

    stash

---
+++ index7.cs
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+using System ;
+namespace Learning{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Index7.cs in dev branch");
+            return ;
+        }
+    }
+    
+}
+
