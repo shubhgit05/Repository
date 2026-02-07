99073d6b2c2a0b8e9257791238ca2289ca511b0f
shubhgit05 <shubh_arora@epam.com>
2026-02-07 15:37:34 +0530

    add index14.cs

---
+++ index14.cs
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System ;
+namespace Learning{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("index12.cs in dev branch");
+            return ;
+        }
+    }
+    
+}
+
+
