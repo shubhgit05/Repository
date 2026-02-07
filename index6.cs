eb2bff50c4a5d333c2af0fb21b2bf5de7fa5228c
shubhgit05 <shubh_arora@epam.com>
2026-02-07 14:05:51 +0530

    add index6.cs

---
+++ index6.cs
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+using System ;
+namespace Learning{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Index6.cs in dev branch");
+            return ;
+        }
+    }
+    
+}
+
