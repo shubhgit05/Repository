49c257e2ae50582a2b8c3b1b42b5e57936f392f4
shubhgit05 <shubh_arora@epam.com>
2026-02-07 14:03:04 +0530

    Revert "change"
    This reverts commit 211a30ae0bc09f8edae830fbfa0897b863250ae8.

--- index.cs
+++ index.cs
@@ -9,7 +9,7 @@ namespace Learning{
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Shubh Arora  ");
+            Console.WriteLine("Shubh Arora");
             return ;
         }
     }
